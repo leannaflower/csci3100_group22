ab9432778d3b6438c8e52dca519369f58a2c6382
Observer <156042391+zymsc03@users.noreply.github.com>
2025-11-19 12:43:24 +0800

    py backend test 2

--- requesttest.py
+++ requesttest.py
@@ -11,6 +11,12 @@ def test_login(username, password):
     print(f"状态码：{response.status_code}")
     print(f"响应内容：{response.json()}")
     return response
+def test_register(username,password):
+    url = f"{BASE_URL}/auth/register"
+    data = {"username": username, "password": password}
+    response = requests.post(url, json=data)
+    print(f"状态码：{response.status_code}")
+    print(f"响应内容：{response.json()}")
 if __name__ == "__main__":
     print("测试正确登录：")
     test_login("test", "testpwd")
@@ -22,3 +28,9 @@ if __name__ == "__main__":
 # 测试用例 3：不存在的用户
     print("\n测试不存在的用户：")
     test_login("nonexistent", "anypassword")
+    print("测试正确注册：")
+    test_register("test1", "password")
+    print("测试正确登录：")
+    test_login("test1", "password")
+    print("测试已注册的用户")
+    test_register("test", "testpwd")
